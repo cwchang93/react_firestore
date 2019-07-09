a768817095e2c63c40542c8a11569f08666beeee
Jinwei Chang <cwchang93@gmail.com>
2019-07-09 08:13:42 +0800

    Add Redux

--- src/features/testArea/testComponent.jsx
+++ src/features/testArea/testComponent.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { incrementCounter, decrementCounter } from './testAction'
 
-class testComponent extends Component {
+const mapState = (state) => ({
+    data: state.data
+})
+
+const actions = {
+    incrementCounter,
+    decrementCounter
+}
+
+class TestComponent extends Component {
     render() {
+        const { data, incrementCounter, decrementCounter } = this.props;
+
         return (
             <div>
                 Test Component
+                <div>The answer is: {data}</div>
+                <button onClick={incrementCounter}>Increment</button>
+                <button onClick={decrementCounter}>Decrement</button>
+
             </div>
         )
     }
 }
 
-export default testComponent
+export default connect(mapState, actions)(TestComponent)
